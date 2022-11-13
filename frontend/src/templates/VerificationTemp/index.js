c18eddfd10d74ff69e997f1db6723bc12307bd59
Temitope3665 <aroyewontope@gmail.com>
2022-11-13 07:01:42 +0100

    modal pop up added onclick of verify

--- frontend/src/templates/VerificationTemp/index.js
+++ frontend/src/templates/VerificationTemp/index.js
@@ -1,10 +1,20 @@
-import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  SimpleGrid,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import VerificationIcon from "../../assets/icons/verification-icon.png";
 import CustomButton from "../../components/CustomButton/customButton";
+import SuccessModal from "../../components/Modal/successModal";
 import TextInput from "../../components/TextInputs/TextInput";
 import LatestNews from "../LatestNews";
 
 const VerificationTemp = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex w="100%">
       <Box w="70%">
@@ -34,6 +44,16 @@ const VerificationTemp = () => {
           <Text fontSize="20px">Enter product ID to verify</Text>
           <Text>Please follow the steps below to authenticate your drugs</Text>
           <TextInput placeholder="Enter unique code" />
+
+          <CustomButton
+            onClick={() => onOpen()}
+            color="brand.white"
+            bg="brand.blue"
+            w="130px"
+            mt="20px"
+          >
+            Verify
+          </CustomButton>
         </Box>
 
         <Box mt="50px" bg="brand.white" p="40px" color="#3A3A3ABF">
@@ -61,6 +81,25 @@ const VerificationTemp = () => {
       <Box w="30%" ml="20px">
         <LatestNews />
       </Box>
+
+      <SuccessModal
+        isOpen={isOpen}
+        onClose={onClose}
+        message="Authetication Invalid"
+        handleNoOnclick={() => onClose()}
+      >
+        <Text>
+          This product has already been purchased and verified by another user.
+        </Text>
+      <Box textAlign="left" mt="20px">
+      <Text fontWeight="600">Product Info</Text>
+        <Text>Product name: Paracetamol</Text>
+        <Text>Product ID: t373dbjdjf</Text>
+        <Text>Manufacturer: Farmson</Text>
+        <Text>Production date: 20/10/2022</Text>
+        <Text>Expiry date: 20/10/2023</Text>
+      </Box>
+      </SuccessModal>
     </Flex>
   );
 };
